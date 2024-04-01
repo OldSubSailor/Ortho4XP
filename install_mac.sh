74521d728ee2f438d3101b7e68dce5198f1b4531
OldSubSailor <54075170+OldSubSailor@users.noreply.github.com>
2024-04-01 10:37:25 -0500

    Update install_mac.sh

--- install_mac.sh
+++ install_mac.sh
@@ -6,4 +6,4 @@ pip3.11 install cython
 pip3.11 install pyproj
 
 # Install other dependencies shapely==1.8.1, numpy==1.23.5
-pip3 install numpy shapely rtree pillow requests
+pip3.11 install numpy shapely rtree pillow requests
